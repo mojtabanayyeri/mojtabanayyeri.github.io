0cd0d2b20f247186ba79ae342c3599303abc6a6e
mojtabanayyeri <44945962+mojtabanayyeri@users.noreply.github.com>
2025-07-16 09:32:53 +0200

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,1 +1,8 @@
-
+// js/main.js
+const links = document.querySelectorAll('nav a');
+for (const link of links) {
+  link.addEventListener('click', e => {
+    e.preventDefault();
+    document.querySelector(link.getAttribute('href')).scrollIntoView({ behavior: 'smooth' });
+  });
+}
